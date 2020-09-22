25ac717eda7315c5bab7618d64e366204c177e45
xren005 <xren005@163.com>
2020-09-22 15:25:01 +0800

    okkkk

--- v1_mvp/src/main/java/com/itp/helloworld.java
+++ v1_mvp/src/main/java/com/itp/helloworld.java
@@ -3,5 +3,6 @@ package com.itp;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world222");
     }
 }
